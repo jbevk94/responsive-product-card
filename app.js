51ff81f7a8bfb3a52ab0b468cb9d443718cc8ba1
Beverly James <jbevk94@gmail.com>
2020-08-27 16:27:12 -0700

    Updated Files

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const sizes = document.querySelectorAll('.size');
 const colors = document.querySelectorAll('.color');
 const shoes = document.querySelectorAll('.shoe');
 const gradients = document.querySelectorAll('.gradient');
+const shoeBg = document.querySelectorAll('.shoeBackground');
+
+let prevColor = "blue";
+let animationEnd = true;
 
 function changeSize(){ //changes the size selected
     sizes.forEach(size => size.classList.remove('active'));
@@ -9,11 +13,17 @@ function changeSize(){ //changes the size selected
 }
 
 function changeColor(){ //change the color selected
+    if(!animationEnd) return;
     let primary = this.getAttribute('primary');//targets hex
 
     let color = this.getAttribute('color');//target clicked button color
 
     let shoe = document.querySelector(`.shoe[color="${color}"]`)//select color shoe image
+
+    let gradient = document.querySelector(`.gradient[color="${color}"]`) 
+
+    let prevGradient = document.querySelector(`.gradient[color="${prevColor}"]`) 
+
 
     colors.forEach(c =>c.classList.remove('active'));
     this.classList.add('active');
@@ -23,8 +33,34 @@ function changeColor(){ //change the color selected
     shoes.forEach(s => s.classList.remove('show'));
     shoe.classList.add('show');
 
-    gradients.forEach(g => g.classList.removed('first'))
+    gradients.forEach(g => g.classList.remove('first'))
+    gradient.classList.add('first', 'second')
+    prevGradient.classList.add('second');
+
+    prevColor = color;
+    animationEnd = false;
+
+    gradient.addEventListener('animationend', () => {
+        animationEnd = true;
+    })
+
 }
 
 sizes.forEach (size => size.addEventListener('click', changeSize)) ;
 colors.forEach(c => c.addEventListener('click', changeColor));
+
+let x = window.matchMedia("(max-width: 1000px)");
+
+function changeHeight(){
+    if(x.matches){
+        let shoeHeight = shoes[0].offsetHeight;
+        shoeBg.style.height = `${shoeHeight * 0.9}px`;
+    }
+    else{
+        shoeBg.style.height = "475px";
+    }
+}
+
+changeHeight();
+
+window.addEventListener('resize', changeHeight);
